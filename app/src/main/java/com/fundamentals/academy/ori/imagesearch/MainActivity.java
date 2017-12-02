ae7d766038a2609d0b99bc9a44c205736864bf60
Ori home computer <ori60907@gmail.com>
2017-12-02 22:35:09 +0200

    Bringing info from web test

--- app/src/main/java/com/fundamentals/academy/ori/imagesearch/MainActivity.java
+++ app/src/main/java/com/fundamentals/academy/ori/imagesearch/MainActivity.java
@@ -2,12 +2,44 @@ package com.fundamentals.academy.ori.imagesearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.ResponseCache;
+
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity{
+
+    static final private String pixabary_api_key = "7251510-2163e90b412235adef065d60e";
+    private static final String TAG = "MainActivityLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PixabayServiceGenerator service_generator = new PixabayServiceGenerator();
+        PixabayServiceGenerator.PixabayService pixabay_service = service_generator.createService();
+
+        retrofit2.Call<ImageSearchResponse> image_search_call = pixabay_service.searchImages(
+                "flowers", "photo"
+        );
+
+        image_search_call.enqueue(new Callback<ImageSearchResponse>() {
+            @Override
+            public void onResponse(retrofit2.Call<ImageSearchResponse> call, Response<ImageSearchResponse> response) {
+                ImageSearchResponse data = response.body();
+                Log.i(TAG, data.getTotalHits().toString());
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<ImageSearchResponse> call, Throwable t) {
+                Log.e(TAG, "Getting images data failed!");
+                t.printStackTrace();
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/fundamentals/academy/ori/imagesearch/PixabayServiceGenerator.java
@@ -0,0 +1,64 @@
+package com.fundamentals.academy.ori.imagesearch;
+
+import java.util.Collections;
+
+import okhttp3.CipherSuite;
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by User on 02/12/2017.
+ */
+
+public class PixabayServiceGenerator {
+    static final String BASE_URL = "https://pixabay.com/api/";
+    static final String PIXABAY_API_KEY = "7251510-2163e90b412235adef065d60e";
+    static ConnectionSpec spec = new
+            ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+            .tlsVersions(TlsVersion.TLS_1_2)
+            .cipherSuites(
+                    CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                    CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                    CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+            .build();
+
+    static OkHttpClient client = new OkHttpClient.Builder()
+            .connectionSpecs(Collections.singletonList(spec))
+            .build();
+    //static OkHttpClient.Builder http_client = new OkHttpClient.Builder();
+
+    static Retrofit.Builder retrofit_builder = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create());
+    static Retrofit retrofit = retrofit_builder.build();
+
+    public PixabayService createService(){
+//        OkHttpClient.Builder http_client = new OkHttpClient.Builder();
+//        Retrofit.Builder retrofit_builder = new Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .client(http_client.build())
+//                .addConverterFactory(GsonConverterFactory.create());
+//
+//        Retrofit retrofit = retrofit_builder.build();
+
+        return retrofit.create(PixabayService.class);
+    }
+
+    public interface PixabayService{
+
+        /** Basic search image query **/
+        @GET("?key="+PIXABAY_API_KEY)
+        Call<ImageSearchResponse> searchImages(
+                @Query("q") String search_query,
+                @Query("image_type") String image_type
+        );
+
+    }
+}
